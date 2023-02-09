b0186877eb5a8057de7b2ce24823604a02f62577
Satvik Kishore <satvik.kishore@gmail.com>
2023-02-09 11:49:54 -0500

    lint

--- app.py
+++ app.py
@@ -5,25 +5,28 @@ app = Flask(__name__)
 
 global_store = ["unknown"]
 
-@app.route('/')
+
+@app.route("/")
 def hello():
-    return 'Welcome to Flask, have fun!'
+    return "Welcome to Flask, have fun!"
 
-@app.route('/input')
+
+@app.route("/input")
 def opt1():
-    user = request.args.get('user')
+    user = request.args.get("user")
     # global_store[0] = user
-    return f'User is {user}'
+    return f"User is {user}"
 
-@app.route('/output')
+
+@app.route("/output")
 def opt2():
-    return f'User is {global_store[0]}'
+    return f"User is {global_store[0]}"
 
 
-@app.route('/square')
+@app.route("/square")
 def opt3(n=None):
     if n is None:
-        n = request.args.get('n')
+        n = request.args.get("n")
         n = int(n)
     # global_store[0] = user
     return str(n**2)
--- test_app.py
+++ test_app.py
@@ -1,8 +1,9 @@
-from app import *
+from app import opt3
+
 
 def test1():
     assert opt3(5) == "25"
 
+
 def test2():
     assert opt3(7) == "49"
-    
