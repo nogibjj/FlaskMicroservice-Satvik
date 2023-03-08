384d8031e6747e1dce7031d6727c1f084113d6ac
Satvik Kishore <satvik.kishore@duke.edu>
2023-03-08 21:11:02 +0000

    dockerize my stuff

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask import request
+import os
 
-app = Flask(__name__)
 
 global_store = ["unknown"]
 
-
+app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Welcome to Flask, have fun!"
@@ -30,3 +30,8 @@ def opt3(n=None):
         n = int(n)
     # global_store[0] = user
     return str(n**2)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+    
